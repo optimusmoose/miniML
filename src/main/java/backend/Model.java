bb5e8eb74dd3da3015b616bf67540fa79bc993f5
dave <david.blasen@umontana.edu>
2017-03-16 22:23:20 -0600

    weka models may be saved, but need to be tested. added some documentation in the process.

--- src/main/java/backend/Model.java
+++ src/main/java/backend/Model.java
@@ -12,35 +12,50 @@ import weka.classifiers.functions.MultilayerPerceptron;
 import weka.classifiers.trees.M5P;
 import weka.classifiers.Evaluation;
 
-public abstract class Model {
-    /*
-    * A general Weka function wrapped in a way that allows us to run it and get results from it.
-    *
-    * Ideally, we will have the entirety of the Model available for inspection after running
-    * and keep it around if it outperforms other solutions. We may also want an approach to
-    * exporting the Model so that it can be reused on other data in the future.
-    */
+//libs for serialization (saving/loading)
+import java.io.Serializable;
+
+/**
+ * A general Weka function wrapped in a way that allows us to run it and get results from it.
+ *
+ * Ideally, we will have the entirety of the Model available for inspection after running
+ * and keep it around if it outperforms other solutions. We may also want an approach to
+ * exporting the Model so that it can be reused on other data in the future.
+ */
+public abstract class Model implements Serializable {
     protected String[] pars;
     protected Instances data;
     protected Evaluation eval;
+    /**
+    * construct and run the Model. Keep as a functioning unit that can be modified as needed.
+    *
+    * @param   d         A Weka Instance of data. The Model assumes that the data will have a class assigned
+    *                    before it gets here.
+    * @param   params    A String[] of parameters understood by the Model. If provided, these will be used
+    *                    to provide the Model with constraints.
+    * @return  void      Other software will be able to get the Evaluation of the Model, but it will not return
+    *                    anything by default.
+    */
     public Model(Instances d, String[] params) throws Exception {
-        /*
-        * construct and run the Model. Keep as a functioning unit that can be modified as needed.
-        *
-        * @param   d         A Weka Instance of data. The Model assumes that the data will have a class assigned
-        *                    before it gets here.
-        * @param   params    A String[] of parameters understood by the Model. If provided, these will be used
-        *                    to provide the Model with constraints.
-        * @return  void      Other software will be able to get the Evaluation of the Model, but it will not return
-        *                    anything by default.
-        */
         data = d;
         pars = params;
+        eval = null;
     }
+
+    /**
+     * Run the model.
+     *
+     * This will 'fill in' the Model's eval, making results available to the user.
+     * Once run, a Model should be savable. This lets a user use a good Model again.
+     *
+     * @throws Exception    Something goes wrong in Models. TODO what goes wrong in Models??
+     */
+    abstract void run() throws Exception;
+
 }
 
 class LR_Model extends Model {
-    /*
+    /**
     * Generates a Weka LinearRegression function Model acting on our data instance with our parameters.
     */
     public LR_Model(Instances d, String[] params) throws Exception {
@@ -48,7 +63,7 @@ class LR_Model extends Model {
         run();
     }
 
-    private void run() throws Exception {
+    protected void run() throws Exception {
         LinearRegression my_lr = new LinearRegression();
         if(pars != null) {
             my_lr.setOptions(pars);
@@ -60,7 +75,7 @@ class LR_Model extends Model {
 }
 
 class NN_Model extends Model {
-    /*
+    /**
     * Generates a Weka MultlayerPerceptron function Model acting on our data instance with our parameters.
     */
     public NN_Model(Instances d, String[] params) throws Exception {
@@ -68,7 +83,11 @@ class NN_Model extends Model {
         run();
     }
 
-    private void run() throws Exception {
+    /**
+     * Run the neural network model
+     * @throws Exception
+     */
+    protected void run() throws Exception {
         MultilayerPerceptron mlp = new MultilayerPerceptron();
         //set our parameters to the object if applicable
         if(pars != null) {
--- src/main/java/backend/WekaInvoker.java
+++ src/main/java/backend/WekaInvoker.java
@@ -1,25 +1,37 @@
 package backend;
 
+import weka.classifiers.Evaluation;
 import weka.core.Instances;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * TODO finish commenting these parts before design pattern gets compromised
+ */
 public class WekaInvoker {
 }
 
-
-/*
-Receiver class.
-This object accepts the instructions and parameters
-to call Weka algorithms.
-
-Ideally, this object will be able to cache a
-parameter/algorithm/result set for easy reporting to
-the main application.
-*/
+/**
+ *
+ *Receiver class.
+ *This object accepts the instructions and parameters
+ *to call Weka algorithms.
+ *
+ *Ideally, this object will be able to cache a
+ *parameter/algorithm/result set for easy reporting to
+ the main application.
+ */
 class WekaTaskManager{
-    private Instances data; //it holds the dataset, too.
+    protected Instances data; //it holds the dataset, too.
+    protected ArrayList<Evaluation> evals = new ArrayList<Evaluation>();
     //private String[] params;
     public void manage_LR(String[] params) {
         System.out.println("Calling linear regression");
@@ -40,6 +52,41 @@ class WekaTaskManager{
     public void setData(Instances d){
         data = d;
     }
+
+    /**
+     * Untested.
+     *
+     * Write a Model to file for future loading.
+     *
+     * @param obj
+     * @param fileName
+     * @throws IOException
+     */
+    public static void serialize(Object obj, String fileName) throws IOException {
+        FileOutputStream fos = new FileOutputStream(fileName);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(obj);
+        fos.close();
+    }
+
+    /**
+     * Untested
+     *
+     * Read a previous Model in from file and make it accessible.
+     *
+     * @param fileName
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static Object deserialize(String fileName) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Object obj = ois.readObject(); //TODO recast as some concrete Model!
+        ois.close();
+        return obj;
+    }
+
 }
 
 // Invoker.
