770244d1ba6b9f6dd31c112df46421be32d4bd71
mpatera <mattpatera1994@gmail.com>
2017-03-20 01:27:06 -0600

    Added skeleton for console output class

---
+++ src/main/java/utils/MessageConsole.java
@@ -0,0 +1,32 @@
+package utils;
+
+import javax.swing.*;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
+import java.awt.*;
+import java.io.PrintStream;
+import java.lang.annotation.Documented;
+
+/**
+ * Created by mattpatera on 3/20/17.
+ */
+public class MessageConsole {
+
+    private JTextArea messageBox;
+    private Document document;
+
+    public MessageConsole(JTextArea messageBox) {
+        this.messageBox = messageBox;
+        this.document = messageBox.getDocument();
+        messageBox.setEditable(false);
+    }
+
+    public void redirectOut(Color textColor, PrintStream printStream) {
+
+    }
+
+    public void redirectError() {
+
+    }
+
+}
