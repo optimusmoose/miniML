0cff483390f919c27bf058a485be6a0bb5d65cae
Zachary Falkner <falkzach@gmail.com>
2017-04-11 20:23:14 -0600

    Improve dataset tab (#13)
    * replace depricated preview pane
    
    * progress on gridbag layout
    
    * gridbag improvements

--- src/main/java/frontend/DatasetTab.java
+++ src/main/java/frontend/DatasetTab.java
@@ -5,26 +5,33 @@ import utils.TypeFactory;
 import workflow.Keys;
 import workflow.WorkflowManager;
 import workflow.context.AbstractCompositeContext;
-import workflow.context.ParameterContext;
 import workflow.context.DatasetContext;
 import workflow.context.FileContext;
+import workflow.context.ParameterContext;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-
-import static org.apache.commons.io.FileUtils.readFileToString;
 
 public class DatasetTab extends JComponent {
 
     private AbstractCompositeContext parentContext;
     private DatasetContext context;
     private FileContext fileSelectContext;
+
     private String dataset;
-    private JTextPane txt = new JTextPane();
+    private String content;
+
+    GridBagConstraints constraints;
+    JPanel panel;
+
+    private JTextPane previewDataset = new JTextPane();
+    private JTextPane previewContent = new JTextPane();
 
     public DatasetTab() {
         super();
@@ -33,27 +40,23 @@ public class DatasetTab extends JComponent {
         context = new DatasetContext(parentContext, Keys.DatasetConfig);
 
         this.setLayout(new GridLayout());
-        JPanel panel = new JPanel(false);
-        panel.setLayout(new GridLayout(6, 0));
-        panel.add(fileSelectPanel());
-        this.add(panel);
+        this.constraints = new GridBagConstraints();
 
+        this.panel = new JPanel(false);
+        this.panel.setLayout(new GridBagLayout());
+        fileSelectPanel();
+        this.add(this.panel);
     }
 
-    private JPanel fileSelectPanel(){
-        //context for the file selector
+    private void fileSelectPanel(){
         fileSelectContext = new FileContext(this.context, Keys.DatasetFile);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout());
-        JScrollPane scrollPane = new JScrollPane(txt);
-        JButton fileSelect = new JButton("Browse...");
-        JLabel tip = new JLabel("Select a Dataset: ");
+        JScrollPane datasetScrollPane = new JScrollPane(previewDataset);
+        JScrollPane contentScrollPane = new JScrollPane(previewContent);
+        JLabel browsLabel = new JLabel("Select a Dataset: ");
+        JButton browseButton = new JButton("Browse...");
 
-        /*
-         * listeners should be anonymous inner classes, this prevents nasty hacks
-         */
-        fileSelect.addActionListener(new ActionListener() {
+        browseButton.addActionListener(new ActionListener() {
             private AbstractCompositeContext context;
 
             @Override
@@ -62,21 +65,34 @@ public class DatasetTab extends JComponent {
                     selectFile();
                     context = WorkflowManager.INSTANCE.getContextByKey(Keys.DatasetFile);
                     handleFileSelectContext((ParameterContext) context);
-                } catch (IOException exception) {
+                } catch (Exception exception) {
                     MiniMLLogger.INSTANCE.exception(exception);
                 }
             }
         });
 
-        panel.add(tip);
-        panel.add(fileSelect);
-        panel.add(scrollPane);
-        return panel;
-    }
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.weightx = 0.33;
+        constraints.weighty = 0.1;
+        this.panel.add(browsLabel, this.constraints);
 
-    //TODO: move this to wrapper magic class that does not exist yet
-    public FileContext getFileSelectContext(){
-        return this.fileSelectContext;
+        this.constraints.gridx = 1;
+        constraints.weightx = 0.4;
+        this.panel.add(browseButton, this.constraints);
+
+        this.constraints.fill = GridBagConstraints.HORIZONTAL;
+        this.constraints.gridx = 2;
+        this.panel.add(datasetScrollPane, this.constraints);
+
+        this.constraints.fill = GridBagConstraints.BOTH;
+        this.constraints.gridx = 0;
+        this.constraints.gridy = 1;
+        this.constraints.gridwidth = 3;
+        this.constraints.weightx = 1;
+        this.constraints.weighty = 0.9;
+
+        this.panel.add(contentScrollPane, this.constraints);
     }
 
     public void handleFileSelectContext(ParameterContext context) {
@@ -85,22 +101,38 @@ public class DatasetTab extends JComponent {
     }
 
     public void selectFile() throws IOException {
-        String contents;
         JFileChooser fileChooser = new JFileChooser();
         File selectedFile;
         fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
         int result = fileChooser.showOpenDialog(this.getParent());
         if (result == JFileChooser.APPROVE_OPTION) {
             selectedFile = fileChooser.getSelectedFile();
-            MiniMLLogger.INSTANCE.info("Selected file: " + selectedFile.getAbsolutePath());
-            contents = readFileToString(selectedFile);//TODO: deprecated fileread, why! this will explode on a huge file!
             this.dataset = selectedFile.getAbsolutePath();
-            txt.setText(contents);
+            this.previewDataset.setText(this.dataset);
+            this.previewData(selectedFile);
+
+            MiniMLLogger.INSTANCE.info("Selected file: " + selectedFile.getAbsolutePath());
         }
     }
 
-    public void previewData() {
+    public void previewData(File file) {
+        String line;
+        int lines = 0;
+        int linesToRead = 250; //TODO: extract to a config
+        BufferedReader reader;
 
+        try {
+            reader = new BufferedReader(new FileReader(file.getAbsolutePath()));
+            while(((line = reader.readLine()) != null) && lines < linesToRead) {
+                this.content += line + '\n';
+                lines++;
+            }
+            reader.close();
+        } catch (IOException exception) {
+            MiniMLLogger.INSTANCE.exception(exception);
+        }
+
+        previewContent.setText(this.content);
     }
 
 }
