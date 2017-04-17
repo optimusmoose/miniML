30be6d1e60d1861f645051b98d00e41d1e15fe78
dblasen <david.blasen@umconnect.umt.edu>
2017-04-17 11:10:16 -0600

    naming conventions No. 431

--- src/main/java/backend/ParameterIFace.java
+++ src/main/java/backend/ParameterIFace.java
@@ -11,73 +11,73 @@ public interface ParameterIFace {
 }
 
 class ParameterIFace_NN implements ParameterIFace {
-    ArrayList<WrappedParam> params;
+    ArrayList<WrappedParam> parameters;
     /**
      * Creates an array of all useful parameters, their mins/maxes, flags, etc.
      * For now, we are skipping: E,G,A,B,C,I,R,D
      * @return
      */
     public ParameterIFace_NN(){
-        ArrayList<WrappedParam> params = new ArrayList<WrappedParam>();
+        ArrayList<WrappedParam> parameters = new ArrayList<WrappedParam>();
         //learning rate
         WrappedParam learnRate = new WrappedParamFloat("float","learning rate","-L",0,1);
-        params.add(learnRate);
+        parameters.add(learnRate);
         //momentum
         WrappedParam momentum = new WrappedParamFloat("float","momentum","-M",0,1);
-        params.add(momentum);
+        parameters.add(momentum);
         //number of epochs
         WrappedParam epochs = new WrappedParamInt("int","number of epochs","-N",1,1000);
-        params.add(epochs);
+        parameters.add(epochs);
         //validation set size
         WrappedParam valid = new WrappedParamInt("int","validation set size","-V",1,99);
-        params.add(valid);
+        parameters.add(valid);
         //seed
         WrappedParam seed = new WrappedParamInt("int","RNG seed","-S",0, Integer.MAX_VALUE - 1);
-        params.add(seed);
+        parameters.add(seed);
         //nodes each layer
         WrappedParam layer = new WrappedParamString("String","layer width","-H","aiot");
-        params.add(layer);
-        this.params = params;
+        parameters.add(layer);
+        this.parameters = parameters;
     }
 
     /**
      * Get all those nice parameters we made above.
-     * @return params   a list of WrappedParam
+     * @return parameters   a list of WrappedParam
      */
     public ArrayList<WrappedParam> getParameters() {
-        return this.params;
+        return this.parameters;
     }
 
 }
 
 
 class ParameterIFace_LR implements ParameterIFace {
-    ArrayList<WrappedParam> params;
+    ArrayList<WrappedParam> parameters;
     /**
      * Creates an array of all useful parameters, their mins/maxes, flags, etc.
      * For now, we are skipping: minimal, additional stats, debug, capabilities
      * @return
      */
     public ParameterIFace_LR(){
-        ArrayList<WrappedParam> params = new ArrayList<WrappedParam>();
+        ArrayList<WrappedParam> parameters = new ArrayList<WrappedParam>();
         //selection method
         WrappedParam selection = new WrappedParamInt("int","Selection method","-S",0,2);
-        params.add(selection);
+        parameters.add(selection);
         //ridge parameter TODO correct window after getting info on ridge
         WrappedParam ridge = new WrappedParamFloat("float","ridge","-R",0,1);
-        params.add(ridge);
+        parameters.add(ridge);
         //eliminate colinear variables
         WrappedParam colinear = new WrappedParamEmpty("empty","Eliminate Colinear","-C");
-        params.add(colinear);
-        this.params = params;
+        parameters.add(colinear);
+        this.parameters = parameters;
     }
 
     /**
      * Get all those nice parameters we made above.
-     * @return params   a list of WrappedParam
+     * @return parameters   a list of WrappedParam
      */
     public ArrayList<WrappedParam> getParameters() {
-        return this.params;
+        return this.parameters;
     }
 
 }
