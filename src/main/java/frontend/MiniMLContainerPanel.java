07e160315fc5c6be2a714bec53238bea7056c2b0
Zachary Falkner <falkzach@gmail.com>
2017-04-19 15:18:41 -0600

    working tab icons java look and feel injection eliminate preprocess tab

--- src/main/java/frontend/MiniMLContainerPanel.java
+++ src/main/java/frontend/MiniMLContainerPanel.java
@@ -8,6 +8,7 @@ import workflow.context.MiniMLContext;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.net.URL;
 
 public class MiniMLContainerPanel extends JPanel {
 
@@ -29,10 +30,14 @@ public class MiniMLContainerPanel extends JPanel {
     {
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.setTabPlacement(SwingConstants.LEFT);
+
         ImageIcon icon = new ImageIcon();
+        Image image;
+        URL path = this.getClass().getResource("img/miniml_icon.png");
 
         try {
-//            icon = new ImageIcon(this.getClass().getResource("img/miniml_logo.png"));//TODO: fix resource acccess for this
+            image = new ImageIcon(path).getImage().getScaledInstance(24, 24, Image.SCALE_SMOOTH);
+            icon = new ImageIcon(image);
         } catch(Exception e) {
             MiniMLLogger.INSTANCE.exception(e);
         }
@@ -41,21 +46,17 @@ public class MiniMLContainerPanel extends JPanel {
         tabbedPane.addTab("Dataset", icon, datasetTab,"Select a dataset for analysis.");
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 
-        PreprocessTab preprocessTab = new PreprocessTab();
-        tabbedPane.addTab("Preprocess", icon, preprocessTab, "Curate data to improve performance.");
-        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-
         ModelTab modelTab = new ModelTab();
         tabbedPane.addTab("Model", icon, modelTab,"Configure MiniML's model usage.");
-        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
 
         AnalyzeTab analyzeTab = new AnalyzeTab();
-        tabbedPane.addTab("Analyze", icon, analyzeTab,"Begin analysis and view results.");
-        tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
+        tabbedPane.addTab("Analyze", icon, analyzeTab, "Begin analysis and view results.");
+        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
 
         AdvancedTab advancedTab = new AdvancedTab();
         tabbedPane.addTab("Advanced Settings", icon, advancedTab,"Change system settings.");
-        tabbedPane.setMnemonicAt(4, KeyEvent.VK_5);
+        tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
 
         this.add(tabbedPane);
     }
