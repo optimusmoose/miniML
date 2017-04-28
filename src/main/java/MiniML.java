a9f2b4b200271c1ef3cbb06ab70ec6b27e9a6fe9
Zachary Falkner <falkzach@gmail.com>
2017-04-27 19:52:33 -0600

    Ui improvements (#19)
    * working tab icons
    java look and feel injection
    eliminate preprocess tab
    
    * settings icon
    
    * clean up magic strings

--- src/main/java/MiniML.java
+++ src/main/java/MiniML.java
@@ -1,21 +1,32 @@
 import frontend.MiniMLFrame;
 import utils.Logging.MiniMLLogger;
-import workflow.Keys;
+
+import javax.swing.*;
 
 public class MiniML {
 
     public static final String APPNAME = "MiniML";
 
     public static void main(String[] args) {
-        MiniMLLogger.INSTANCE.info("Starting " + APPNAME);
+        MiniMLLogger.INSTANCE.info("Starting " + APPNAME + ".");
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        }
+        catch (Exception e) {
+            MiniMLLogger.INSTANCE.debug("Failed to set the Java Look and Feel.");
+            MiniMLLogger.INSTANCE.exception(e);
+        }
 
         try {
             MiniMLFrame mainFrame = new MiniMLFrame(APPNAME);
         } catch(Exception e) {
+            MiniMLLogger.INSTANCE.debug("Failed to load the MiniML SWING GUI.");
             MiniMLLogger.INSTANCE.exception(e);
+            System.exit(1);
         }
 
-        MiniMLLogger.INSTANCE.info(APPNAME + " is Ready");
+        MiniMLLogger.INSTANCE.info(APPNAME + " is Ready.");
     }
 
 }
--- src/main/java/frontend/MiniMLContainerPanel.java
+++ src/main/java/frontend/MiniMLContainerPanel.java
@@ -8,11 +8,16 @@ import workflow.context.MiniMLContext;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.net.URL;
 
 public class MiniMLContainerPanel extends JPanel {
 
     private static final int APP_WIDTH = 1200;
     private static final int APP_HEIGHT = 600;
+
+    private static final int ICON_SIZE = 32;
+    private static final String ICON_PATH = "img/miniml_icon.png";
+    private static final String SETTINGS_ICON_PATH = "img/settings_icon.png";
 
     public MiniMLContainerPanel() {
         super(new GridLayout());
@@ -29,10 +34,19 @@ public class MiniMLContainerPanel extends JPanel {
     {
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.setTabPlacement(SwingConstants.LEFT);
+
         ImageIcon icon = new ImageIcon();
+        ImageIcon settingsIcon = new ImageIcon();
+        Image image;
+        URL iconPath = this.getClass().getResource(ICON_PATH);
+        URL settingsIconPath = this.getClass().getResource(SETTINGS_ICON_PATH);
 
         try {
-//            icon = new ImageIcon(this.getClass().getResource("img/miniml_logo.png"));//TODO: fix resource acccess for this
+            image = new ImageIcon(iconPath).getImage().getScaledInstance(ICON_SIZE, ICON_SIZE, Image.SCALE_SMOOTH);
+            icon = new ImageIcon(image);
+
+            image = new ImageIcon(settingsIconPath).getImage().getScaledInstance(ICON_SIZE, ICON_SIZE, Image.SCALE_SMOOTH);
+            settingsIcon = new ImageIcon(image);
         } catch(Exception e) {
             MiniMLLogger.INSTANCE.exception(e);
         }
@@ -41,21 +55,17 @@ public class MiniMLContainerPanel extends JPanel {
         tabbedPane.addTab("Dataset", icon, datasetTab,"Select a dataset for analysis.");
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 
-        PreprocessTab preprocessTab = new PreprocessTab();
-        tabbedPane.addTab("Preprocess", icon, preprocessTab, "Curate data to improve performance.");
-        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
-
         ModelTab modelTab = new ModelTab();
         tabbedPane.addTab("Model", icon, modelTab,"Configure MiniML's model usage.");
-        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
 
         AnalyzeTab analyzeTab = new AnalyzeTab();
-        tabbedPane.addTab("Analyze", icon, analyzeTab,"Begin analysis and view results.");
-        tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
+        tabbedPane.addTab("Analyze", icon, analyzeTab, "Begin analysis and view results.");
+        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
 
         AdvancedTab advancedTab = new AdvancedTab();
-        tabbedPane.addTab("Advanced Settings", icon, advancedTab,"Change system settings.");
-        tabbedPane.setMnemonicAt(4, KeyEvent.VK_5);
+        tabbedPane.addTab("Advanced Settings", settingsIcon, advancedTab,"Change system settings.");
+        tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
 
         this.add(tabbedPane);
     }
